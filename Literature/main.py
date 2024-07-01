624c6c981848a9dd8c6f3b442f1296f48677f733
ivanya123 <aplesnin2010@yandex.ru>
2024-07-02 02:02:27 +0300

    try - except main.py

--- Literature/main.py
+++ Literature/main.py
@@ -10,9 +10,15 @@ if __name__ == '__main__':
         while string != '}':
             string = input()
             final_string = final_string + '\n' + string
-
-        literature = Literature(final_string)
-        with open('Gost.txt', 'a') as f:
-            f.write(literature.gost_literature()+'\n')
-        print(f'Записалось в файл Gost.txt\n{literature.gost_literature()}\nПродолжите ввод для выхода введите end')
+        try:
+            literature = Literature(final_string)
+            with open('Gost.txt', 'a') as f:
+                f.write(literature.gost_literature()+'\n')
+                print(
+                    f'Записалось в файл Gost.txt\n{literature.gost_literature()}\nНажмите enter для продолжения '
+                    f'для выхода введите end')
+        except:
+            print('Неверный формат BibTex\n'
+                  'Input enter для продолжения\n'
+                  'end - для выхода')
         string = input()
