af0b8b18d1151067bdde1246f38e46f766ca92aa
Polina <apl@ni.com>
2024-07-03 00:05:51 +0300

    Комментарий для очень злого, но любимого Ванечки

--- Literature/class_literature.py
+++ Literature/class_literature.py
@@ -2,7 +2,7 @@ import re
 
 
 class Literature:
-    #Ванечка самый лучший
+    #Ванечка злой!!! (но я его люблю)
     def __init__(self, str_from_bibtex):
         self.title = None
         self.author = None
